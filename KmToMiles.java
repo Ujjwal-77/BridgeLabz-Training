a9f69b60a5a07c2f2a407d913da759f7f90f6609
Ujjwal-77 <156050168+Ujjwal-77@users.noreply.github.com>
2025-02-07 10:27:18 +0530

    KmToMiles.java

--- KmToMiles.java
+++ KmToMiles.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class KmToMiles{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        int kilometers=sc.nextInt();
+        double kilometers=sc.nextInt();
         System.out.print("Distance in Miles: "+kilometers*0.621371);
     }
 }
